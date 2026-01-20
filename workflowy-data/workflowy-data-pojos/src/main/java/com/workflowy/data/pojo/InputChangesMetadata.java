eb615b68a7f82ba5c27f985b8cfc44461cf9e41c
Craig P. Motlin <cmotlin@gmail.com>
2026-01-20 10:18:42 -0500

    Add mn (modified by) field to InputChangesMetadata.
    The changes metadata can include both ct (created by) and mn (modified by)
    entries tracking who created and last modified an item.

--- workflowy-data/workflowy-data-pojos/src/main/java/com/workflowy/data/pojo/InputChangesMetadata.java
+++ workflowy-data/workflowy-data-pojos/src/main/java/com/workflowy/data/pojo/InputChangesMetadata.java
@@ -1,11 +1,16 @@
 package com.workflowy.data.pojo;
 
+import java.util.Map;
+
 import javax.annotation.Nullable;
 
 /**
  * Tracks changes/attribution metadata for an item.
+ * Each entry contains a "by" field (user ID) and dynamic fields mapping user IDs to timestamps.
  */
 public record InputChangesMetadata(
 	/** Created by - tracks who created this item. */
-	@Nullable InputChangeEntry ct
+	@Nullable Map<String, Object> ct,
+	/** Modified by - tracks who last modified this item. */
+	@Nullable Map<String, Object> mn
 ) {}

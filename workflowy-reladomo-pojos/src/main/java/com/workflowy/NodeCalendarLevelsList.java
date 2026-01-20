ab781f9ba96e9c5ea75883ad08a4ccf54af73a52
Craig P. Motlin <cmotlin@gmail.com>
2026-01-20 11:45:46 -0500

    Add NodeCalendarLevels class to track calendar hierarchy levels.

---
+++ workflowy-reladomo-pojos/src/main/java/com/workflowy/NodeCalendarLevelsList.java
@@ -0,0 +1,25 @@
+package com.workflowy;
+import com.gs.fw.finder.Operation;
+import java.util.*;
+public class NodeCalendarLevelsList extends NodeCalendarLevelsListAbstract
+{
+	public NodeCalendarLevelsList()
+	{
+		super();
+	}
+
+	public NodeCalendarLevelsList(int initialSize)
+	{
+		super(initialSize);
+	}
+
+	public NodeCalendarLevelsList(Collection c)
+	{
+		super(c);
+	}
+
+	public NodeCalendarLevelsList(Operation operation)
+	{
+		super(operation);
+	}
+}
